bfd722972d844c6c379d0656e070f9a600973e04
DISN-kolo <arrtemsky@gmail.com>
2025-09-14 19:35:29 +0200

    sens generalized, movement template added

--- chars/pc.gd
+++ chars/pc.gd
@@ -2,10 +2,20 @@ extends CharacterBody3D;
 
 @onready var head_pc: Node3D = $HeadPC;
 @onready var camera_pc: Camera3D = $HeadPC/CameraPC;
-const SENSITIVITY = 0.003;
 
-func _unhandled_input(event):
+var input_dir: Vector2 = Vector2(0, 0);
+var direction: Vector3 = Vector3(0, 0, 0);
+var speed_modifier: float = 500.0;
+
+func _unhandled_input(event) -> void:
 	if event is InputEventMouseMotion:
-		head_pc.rotate_y(-event.relative.x * SENSITIVITY);
-		camera_pc.rotate_x(event.relative.y * SENSITIVITY);
+		head_pc.rotate_y(-event.relative.x * Settings.sensitivity);
+		camera_pc.rotate_x(-event.relative.y * Settings.sensitivity);
 		camera_pc.rotation.x = clamp(camera_pc.rotation.x, -PI/2, PI/2);
+
+func _physics_process(delta: float) -> void:
+	input_dir = Input.get_vector("mov_left", "mov_right", "mov_up", "mov_down");
+	direction = (head_pc.transform.basis * Vector3(input_dir.x, 0, input_dir.y));
+	velocity = direction * speed_modifier * delta;
+	print(velocity);
+	move_and_slide();
--- misc/default_empty.gd
+++ misc/default_empty.gd
@@ -2,12 +2,21 @@ extends Node3D;
 @onready var world0 = preload("res://worlds/world_0.tscn");
 var worldinstance;
 @onready var PC = preload("res://chars/pc.tscn");
-var playerinstance;
+var playerinstance: CharacterBody3D;
 
 func _ready() -> void:
 	worldinstance = world0.instantiate();
 	self.add_child(worldinstance);
 	playerinstance = PC.instantiate();
+	playerinstance.position.y += 1;
 	self.add_child(playerinstance);
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
 	pass;
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit();
+	elif event.is_action_pressed("sens_up"):
+		Settings.sensitivity += 0.001;
+	elif event.is_action_pressed("sens_down"):
+		Settings.sensitivity -= 0.001;
